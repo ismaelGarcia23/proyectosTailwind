86b506d56b9e5520963defe35907de8ee02a41d5
ISMAEL ENRIQUE GARCIA HERNANDEZ <ismaelhernan1237@gmail.com>
2025-02-12 23:04:32 -0600

    Mi primer commit

---
+++ webProject-tailwind/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+        <h1 className="p-20 text-6xl text-white font-bold text-center">Hola mundo</h1>
+
+      <div className="my-28 flex justify-center ">
+          <button className="p-5 font-bold text-3xl text-white  bg-sky-500 hover:bg-sky-700 rounded-2xl">
+              Guardar Cambios
+        </button>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
